0a24b3658d27bf1b05f1c722c534d0dc451d9074
uday kumar <uday33379@gmail.com>
2024-07-17 07:55:43 +0530

    Day-1 Tasks

---
+++ Day_01/Variables-Data-Types.js
@@ -0,0 +1,39 @@
+
+//Task-1
+var num=10
+console.log(num);
+//Task-2
+let str="hello world"
+console.log(str);
+//Task-3
+const num1=10;
+console.log(num1);
+//Task-4
+let num2=10; //number
+let str2="hello"//string
+var IsloggedIn=true//boolean
+let car ={
+    name:"tata",
+    model:"nexon",
+    color:"balck"
+}//object
+
+var cars=["tata","hyundai","bmw","bugatti"]
+
+console.log(typeof(num));
+console.log(typeof(str2));
+console.log(typeof(IsloggedIn));
+console.log(typeof(car));
+console.log(typeof(cars));
+
+//Task-5
+let tem=10
+console.log(tem);
+tem=20
+console.log(tem);
+
+//Task-6
+const hello=50
+console.log(hello);
+hello=20
+console.log(hello);
